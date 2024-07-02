606eebbd752a859a060ec890225f9f4d781463e2
firepunch <firepunch119@gmail.com>
2024-07-02 14:01:49 -0700

    Fetch package details

--- src/app/containers/package-view/package-view.component.ts
+++ src/app/containers/package-view/package-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Package } from '../../models/Package';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'package-view',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class PackageViewComponent implements OnInit {
+  package?: Package = undefined;
+  httpService = inject(HttpService)
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    const TEST_PACKAGE_ID = 487303
+    this.getPackage(TEST_PACKAGE_ID)
   }
-
+  getPackage(id:number) {
+    this.httpService.getPackage(id).subscribe((result: Package) => {
+      this.package = result;
+    });
+  }
 }
