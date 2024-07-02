682f627ad1d86dd4fd2e2ecf145fa2f9efce5f20
firepunch <firepunch119@gmail.com>
2024-07-02 13:58:52 -0700

    Add routing to PackageView

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PackageViewComponent } from './containers/package-view/package-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'package-view', pathMatch: 'full' },
+  { path: 'package-view', component: PackageViewComponent },
+];
---
+++ src/app/containers/package-view/package-view.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'package-view',
+  templateUrl: './package-view.component.html',
+  styles: []
+})
+export class PackageViewComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
