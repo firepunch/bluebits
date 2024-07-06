cfb41d0e1be33a088b2e133c5cd5903e96937cbd
firepunch <firepunch119@gmail.com>
2024-07-05 20:50:11 -0700

    Replace bootstrap by ionic

--- src/app/containers/package-view/package-view.component.ts
+++ src/app/containers/package-view/package-view.component.ts
@@ -3,19 +3,26 @@ import { Package } from '../../models/Package';
 import { HttpService } from '../../services/http.service';
 import { Delivery } from '../../models/Delivery';
 import { format } from 'date-fns';
-import { IonContent, IonButton, IonBadge } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { chevronBack } from 'ionicons/icons';
+import {
+  IonButton, IonContent, IonBadge, IonIcon, IonGrid, IonRow, IonCol
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'package-view',
   templateUrl: './package-view.component.html',
   styleUrl: './package-view.component.scss',
   standalone: true,
-  imports: [IonContent, IonButton, IonBadge]
+  imports: [IonButton, IonContent, IonBadge, IonIcon, IonGrid, IonRow, IonCol]
 })
 export class PackageViewComponent implements OnInit {
   package?: Package = undefined;
   delivery?: Delivery = undefined;
   httpService = inject(HttpService)
+  constructor() {
+    addIcons({ chevronBack })
+  }
   ngOnInit(): void {
     const TEST_PACKAGE_ID = 487303
     this.getPackage(TEST_PACKAGE_ID)
