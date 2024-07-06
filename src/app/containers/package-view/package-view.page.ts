51f2cd22310d35018ba0ca5882bcf42ec22504cf
firepunch <firepunch119@gmail.com>
2024-07-05 23:36:57 -0700

    Implement function for copying pickup code

--- src/app/containers/package-view/package-view.page.ts
+++ src/app/containers/package-view/package-view.page.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { format } from 'date-fns';
 import { addIcons } from 'ionicons';
-import { chevronBack, homeOutline, mailOutline } from 'ionicons/icons';
+import { chevronBack, copyOutline, homeOutline, mailOutline } from 'ionicons/icons';
 import { BannerButton } from '../../components/banner-button/banner-button.components';
 import { CompartmentItem } from '../../components/compartment-item/compartment-item.components';
 import { Delivery } from '../../models/Delivery';
 import { Package } from '../../models/Package';
 import { HttpService } from '../../services/http.service';
+import { Clipboard } from '@capacitor/clipboard';
 import {
   IonBadge,
   IonButton,
@@ -15,7 +16,8 @@ import {
   IonContent,
   IonGrid,
   IonIcon,
-  IonRow
+  IonRow,
+  IonToast
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -23,14 +25,14 @@ import {
   templateUrl: './package-view.page.html',
   styleUrl: './package-view.page.scss',
   standalone: true,
-  imports: [CommonModule, IonButton, IonContent, IonBadge, IonIcon, IonGrid, IonRow, IonCol, CompartmentItem, BannerButton]
+  imports: [CommonModule, IonButton, IonContent, IonBadge, IonIcon, IonGrid, IonRow, IonCol, IonToast, CompartmentItem, BannerButton]
 })
 export class PackageViewPage implements OnInit {
   package?: Package;
   delivery?: Delivery;
   httpService = inject(HttpService)
   constructor() {
-    addIcons({ chevronBack, homeOutline, mailOutline })
+    addIcons({ chevronBack, homeOutline, mailOutline, copyOutline })
   }
   ngOnInit(): void {
     const TEST_PACKAGE_ID = 487303
@@ -41,6 +43,11 @@ export class PackageViewPage implements OnInit {
       this.package = result;
       this.delivery = result.delivery_details;
       this.delivery.delivered_at = format(result.delivery_details.delivered_at, 'yyyy-MM-dd hh:mm')
+    });
+  }
+  async onCopyPickupCode() {
+    await Clipboard.write({
+      string: this.delivery?.compartment.pickup_code
     });
   }
 }
