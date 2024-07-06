8e3b0d7dea47544791dc058001e189aac63e7620
firepunch <firepunch119@gmail.com>
2024-07-05 20:49:30 -0700

    Set ionic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IonHeader, IonToolbar, IonBackButton, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, IonHeader, IonToolbar, IonBackButton, IonContent]
 })
 export class AppComponent {
   title = 'bluebits';
